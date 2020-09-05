1a3069a1e31d192d64f466231068f70a09fd6d45
JiahangGu <gujiahang@bupt.edu.cn>
2020-09-05 23:46:18 +0800

    Solve 2 problems of double weekly contest at 2020-9-05

---
+++ Weekly Contest/20-9-05/3-5493-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @Time:2020/9/5 22:59
+# @Author:JiahangGu
+from typing import List
+
+
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+
---
+++ Weekly Contest/20-9-05/4-5494-count-all-possible-routes.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# @Time:2020/9/5 23:21
+# @Author:JiahangGu
+from typing import List
+
+
+class Solution:
+    def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
+        def dfs(cur_city, left_fuel, path):
+            nonlocal ans
+            if left_fuel < 0:
+                return
+            if cur_city == finish:
+                # print(path)
+                ans = (ans + 1) % (10 ** 9 + 7)
+            for i in range(0, len(locations)):
+                if i != cur_city:
+                    path.append(i)
+                    dfs(i, left_fuel - abs(locations[i] - locations[cur_city]), path)
+                    path.pop()
+
+        ans = 0
+        dfs(start, fuel, [start])
+        return ans
+
+
+s = Solution()
+print(s.countRoutes([1,2,3], 0, 2, 40))
