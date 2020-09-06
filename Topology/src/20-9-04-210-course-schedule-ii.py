e82ce378340e60ff12db63da6288815411400efb
JiahangGu <gujiahang@bupt.edu.cn>
2020-09-06 22:32:55 +0800

    Solve topology sort problem of course schedule at 2020-9-06

--- Topology/src/20-9-04-210-course-schedule-ii.py
+++ Topology/src/20-9-04-210-course-schedule-ii.py
@@ -15,6 +15,56 @@ class Solution:
         :return:
         """
         # BFS
-
+        # from collections import defaultdict
+        # edges = defaultdict(list)
+        # in_degree = defaultdict(int)
+        # for item in prerequisites:
+        #     edges[item[1]].append(item[0])
+        #     in_degree[item[0]] += 1
+        # q = [x for x in range(numCourses) if in_degree[x] == 0]
+        # ans = []
+        # while q:
+        #     cur = q.pop(0)
+        #     ans.append(cur)
+        #     for x in edges[cur]:
+        #         in_degree[x] -= 1
+        #         if in_degree[x] == 0:
+        #             q.append(x)
+        # if len(ans) != numCourses:
+        #     return []
+        # return ans
 
         # DFS
+        def dfs(cur):
+            nonlocal cycle_exist
+            visited[cur] = 1
+            for x in edges[cur]:
+                if visited[x] == 1:
+                    cycle_exist = True
+                    return
+                elif visited[x] == 0:
+                    dfs(x)
+                    if cycle_exist:
+                        return
+            ans.append(cur)
+            visited[cur] = 2
+
+        from collections import defaultdict
+        edges = defaultdict(list)
+        out_degree = defaultdict(int)
+        for item in prerequisites:
+            edges[item[1]].append(item[0])
+            out_degree[item[1]] += 1
+        visited = [0] * numCourses
+        ans = []
+        cycle_exist = False
+        for i in range(numCourses):
+            if visited[i] == 0:
+                dfs(0)
+        if cycle_exist:
+            return []
+        return ans[::-1]
+
+
+s = Solution()
+print(s.findOrder(2, [[1,0]]))
