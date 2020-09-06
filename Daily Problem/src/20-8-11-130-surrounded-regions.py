2b4f99f28977ad9062ed990a987b9e336ba95e6a
JiahangGu <gujiahang@bupt.edu.cn>
2020-09-06 22:14:42 +0800

    Solve daily problem of 2020-8-02 at 2020-9-06

--- Daily Problem/src/20-8-11-130-surrounded-regions.py
+++ Daily Problem/src/20-8-11-130-surrounded-regions.py
@@ -2,3 +2,15 @@
 # encoding: utf-8
 # @Time:2020/9/4 15:50
 # @Author:JiahangGu
+from typing import List
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        首先使用搜索找出所有被分离的联通区域，并使用并查集算法标号。然后遍历边界找出在边界的0点，并且记录
+        对应标号，则这些标号都不会修改为X，那么剩余的区域的O都要修改为X。
+        :param board:
+        :return:
+        """
+        def find(x):
