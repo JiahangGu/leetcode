da015cac8ab215d8e50cc3517e10d15076a77675
JiahangGu <gujiahang@bupt.edu.cn>
2020-09-08 14:55:55 +0800

    Solve daily problem of 2020-9-07 at 2020-9-08

--- Daily Problem/src/20-9-08-77-combinations.py
+++ Daily Problem/src/20-9-08-77-combinations.py
@@ -2,3 +2,14 @@
 # encoding: utf-8
 # @Time:2020/9/8 11:07
 # @Author:JiahangGu
+from typing import List
+
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        """
+
+        :param n:
+        :param k:
+        :return:
+        """
