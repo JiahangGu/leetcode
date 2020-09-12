e6f913e6f6aa91f57d85a0fc747b7494fa3d3a20
JiahangGu <gujiahang@bupt.edu.cn>
2020-09-12 09:32:26 +0800

    Solve daily problem of 2020-9-12 at 2020-9-12

--- Daily Problem/src/20-8-01-632-smallest-range-covering-elemnts-from-k-lists.py
+++ Daily Problem/src/20-8-01-632-smallest-range-covering-elemnts-from-k-lists.py
@@ -2,3 +2,13 @@
 # encoding: utf-8
 # @Time:2020/9/3 16:22
 # @Author:JiahangGu
+from typing import List
+
+
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        """
+
+        :param nums:
+        :return:
+        """
